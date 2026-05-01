417255722e4445ab0e46f4d0322b86694dd8d35e
Dipank-Rangi <dipank7025@iitg.ac.in>
2026-05-01 10:55:15 +0000

    02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict "; // treat all JS code as newer version
+
+//alert(3+3) we are using node js not browser it will work in browser
+
+let name = "DIPANK"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint => for very large numbers
+// string => use in double quotes ""
+// boolean => false / true
+// null => stand alone value
+// undefined => value is not declared but variable is declared
+// symbol => to find unique
+
+
+// very imp -> null is a object while undefined is a undefined 
+
+
+// object
+
+
+console.log(typeof(12)); // to find 
+console.log(typeof(name)); // inside is variable name 
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
