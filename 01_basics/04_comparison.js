f8d7e1042d23e947ec1a079b23cb704aa719e2c4
Dipank-Rangi <dipank7025@iitg.ac.in>
2026-05-08 16:37:19 +0000

    04_comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2<1);
+
+// console.log(null == 0);
+// console.log(null <=  0); // >= && <= thy will convert null to 0 so its answer is true 
+
+//console.log(undefined <= 0); it is always always use any comparison
+// console.log();
+
+// === it will also check datatype
+
+// console.log("2"==2); -> true  , it will string to number 
+//console.log("2"===2); -> false because it wll also check the datatype
+
+
