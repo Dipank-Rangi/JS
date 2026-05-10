1fcc5a7dea7fa49c5e1e868c137171c497f442e7
Dipank-Rangi <dipank7025@iitg.ac.in>
2026-05-10 17:17:53 +0000

    string

---
+++ 01_basics/06_strng.js
@@ -0,0 +1,11 @@
+const name = "DIPANK"
+const repoCount = 50
+
+//console.log( repoCount +name + "value"); -> this syntax is outdated dont use '+' this symbol
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`); // use backticks option+tab k uppar wala button , this is string interplation in this we make place holders and insert them at any plaace
+
+const stringName = new String ("DIPANK_RANGI") // another way to declare the string  hee it is a object
+//console.log(typeof(stringName)); -> object
+
+console.log(stringName[1]); // string same as in c++
