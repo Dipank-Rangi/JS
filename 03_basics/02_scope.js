a5c930886e122aa3fb0fb7f2fdbd2ba887e37577
Dipank-Rangi <dipank7025@iitg.ac.in>
2026-05-19 17:05:39 +0000

    scope intro

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,16 @@
+// global scope
+
+if("dip"){
+    //block scope
+    let a = 10
+    const b = 20
+    var c = 30
+
+}
+// var ko kahi bhi declare kro vo hr jgh accessible hai 
+
+let x =[1,2,3]
+if(true){
+    x.push(4)
+}
+console.log(x);
