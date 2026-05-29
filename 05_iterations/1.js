52cd0a0541f49d1863e5f112b6f6448c862d71b4
Dipank-Rangi <dipank7025@iitg.ac.in>
2026-05-29 18:25:01 +0000

    loops

---
+++ 05_iterations/1.js
@@ -0,0 +1,47 @@
+// for
+
+for(let i = 0 ; i < 10 ; i++){
+   // console.log(i);
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+   //console.log(`Table of ${i}`);
+   
+    
+   for (let j = 1; j <=10; j++) {
+    //console.log(`${i} * ${j} = ${i*j}`);
+   }
+    
+}
+
+let array = [1,2,3,4,5,6,7,8,9];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    //console.log(element);
+    
+    
+}
+
+
+// break continue
+
+let i = 0;
+while(i<array.length){
+    //console.log(i);
+    //console.log(array[i]);
+    
+    i++;
+    
+}
+
+i = 10;
+do{
+   // console.log("HII");
+    i++;
+    
+}while(i<=11);
+
+
+
+
