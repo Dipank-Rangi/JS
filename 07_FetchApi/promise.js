4141439d3e18e22015487dd6dac0af6fef98223d
Dipank-Rangi <dipank7025@iitg.ac.in>
2026-06-29 13:40:17 +0000

    fetch api theory

--- 07_FetchApi/promise.js
+++ 07_FetchApi/promise.js
@@ -350,3 +350,11 @@ async function getAllUsers(){
 }
 
 getAllUsers();
+
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
+  return response.json();
+}).then((data)=>{
+  console.log(data);
+  
+}).catch((error) => console.log(error)
+)
