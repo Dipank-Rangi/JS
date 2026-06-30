5b51b995d8bbf99d6ebd3e0f3eab15e68fceb0bd
Dipank-Rangi <dipank7025@iitg.ac.in>
2026-06-30 19:09:11 +0000

    revision

--- 05_iterations/6.js
+++ 05_iterations/6.js
@@ -13,4 +13,9 @@ const myNum = num.filter((num)=>{
 // console.log(myNum);
 
 
+const arr = [1,2,3,4,5,6,7,8,9]
+
+const newNums = arr.map((num)=> num*10).map((num)=>num+1).filter((num)=> num > 40)
+console.log(newNums);
+
  
